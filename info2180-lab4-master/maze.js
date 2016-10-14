e83775a42f94848c56661877fc85946380ccf129
Jonathan Wilson <j.a.wilson96@hotmail.com>
2016-10-14 02:02:48 -0500

    exercise 2

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,7 +1,16 @@
 window.onload = function() {
-    document.getElementById("boundary1").onmouseenter = function() {overBoundary()};
+    document.getElementById("boundary1").onmouseenter = function() {overBoundaryExc2()};
+    //document.getElementById("boundary").onmouseenter = function() {overBoundaryExc2()};
+    //document.getElementsByClassName("boundary").onmouseenter = function() {overBoundaryExc2()};
 };
 
-function overBoundary() {
+function overBoundaryExc1() {
     document.getElementById("boundary1").className+=' youlose';
+}
+
+function overBoundaryExc2() {
+    var walls = document.querySelectorAll(".boundary");
+    for (var i = 0; i < walls.length - 1; i++) {
+      walls[i].className+=' youlose';
+    }
 }
